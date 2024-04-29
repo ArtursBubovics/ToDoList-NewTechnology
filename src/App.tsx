23b9baef5c7bd3a17f8abfdcf9a9384ecdaefc90
ArtursBubovics <artursbubovocs@gmail.com>
2024-04-29 15:19:18 +0300

    Added loader

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import Box from '@mui/material/Box';
 import ToDoLists from './components/To-do-Lists/TodoUnitedBlock';
 import Profile from './components/Profile/ProfileUnitedBlock';
 import { Login } from './components/Login/Login';
-import { BrowserRouter as Router , Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { LinearProgress } from '@mui/material';
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
           <Route path="/ToDoLists" element={<ToDoLists />} />
           <Route path="/Profile" element={<Profile />} />
         </Routes>
+        <LinearProgress variant="determinate" value={20} sx={{position: 'absolute', zIndex: 1, top: 0, right: 0, left: 0, height: '3px'}}/>
       </Box>
     </Router>
   );
