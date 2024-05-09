3a6c965d0d365c21b7ddeab39b2d20b99c38f70a
ArtursBubovics <artursbubovocs@gmail.com>
2024-05-09 15:51:55 +0300

    Added a separator between text and button to ArchiveBlock blocks. Made the bottom part more beautiful

--- src/components/Archive/ArchiveBlock/ArchiveBlock.tsx
+++ src/components/Archive/ArchiveBlock/ArchiveBlock.tsx
@@ -1,20 +1,21 @@
-import { Box,Paper,Typography } from "@mui/material"
+import { Box,Paper,Typography,Divider } from "@mui/material"
 import { RegularButtons } from "../../../common/Buttons/RegularButtons/RegularButtons"
 
 export const ArchiveBlock = ({ text }: { text: string }) => {
     return (
         <Box sx={{ display: 'flex', justifyContent: 'center', alignContent: 'center' }}>
-        <Paper sx={{ width: '95%', height: '90%', marginBottom: '15px', marginTop: '5px', padding: '18px 18px 10px', fontSize: '18px' }} elevation={2}>
-            <Typography sx={{ fontSize: '18px', paddingBottom: '5px' }}>
+        <Paper sx={{ width: '95%', height: '90%', marginBottom: '15px', marginTop: '5px', padding: '18px 18px 8px', fontSize: '18px' }} elevation={2}>
+            <Typography sx={{ fontSize: '18px', paddingBottom: '10px' }}>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt nam provident rerum quaerat animi ducimus reprehenderit magnam. Id, quibusdam vel.
                 {text}
             </Typography>
-            <Box sx={{ display: 'flex', justifyContent: 'space-around', width: '90%' }}>
+            <Divider/>
+            <Box sx={{ display: 'flex', justifyContent: 'space-around', width: '90%', paddingTop: '8px' }}>
                 <RegularButtons sx={{ padding: '3px', fontSize: '13px' }}>
-                    Вернуть
+                    Return
                 </RegularButtons>
                 <RegularButtons sx={{ padding: '3px', fontSize: '13px' }}>
-                    Удалить
+                    Delete
                 </RegularButtons>
             </Box>
         </Paper>
