121d61ff9dacf06189a69edb3d1f530f6c673bf7
ArtursBubovics <artursbubovocs@gmail.com>
2024-05-13 13:59:23 +0300

    Added possibility to expand input field at ToDoListField

--- src/components/To-do-Lists/TodoListsBlock/TodoFields/TodoField.tsx
+++ src/components/To-do-Lists/TodoListsBlock/TodoFields/TodoField.tsx
@@ -2,14 +2,21 @@ import Checkbox from '@mui/material/Checkbox';
 import Box from "@mui/material/Box"
 import { useState } from 'react';
 import FieldsIcons from './FieldsIcons/FieldsIcons';
+import { TextField } from '@mui/material';
 
 const TodoField = () => {
 
     const [isHoverField, updateHoverField] = useState(false)
+    const [multiline, setMultiline] = useState(false);
 
     const changeHoverField = () => {
         updateHoverField(!isHoverField)
     }
+
+    const handleToggleMultiline = () => {
+        setMultiline(!multiline);
+    };
+
 
     return (
         <Box sx={{ paddingBottom: '20px', '&:last-child': { paddingBottom: '0' } }} onMouseEnter={changeHoverField} onMouseLeave={changeHoverField}>
@@ -17,15 +24,31 @@ const TodoField = () => {
                 <Box sx={{ width: '3%', display: 'flex', alignItems: 'center', borderRight: '2px solid #C3C3C3', paddingRight: '1.5%' }}>
                     <Checkbox sx={{ width: '16px', height: '16px' }} />
                 </Box>
-                <Box sx={{ width: '92%', padding: '0% 2%' }}>
-                    <input type='text' style={{ width: '100%', height: '100%', border: 'none', outline: 'none', fontSize: '20px', backgroundColor: '#FFFFFF', paddingTop: '2.2px', textOverflow: 'ellipsis' }}></input>
+                <Box sx={{ width: '92%', height: '100%', padding: '0% 2%' }}>
+                    <TextField
+                        fullWidth
+                        multiline={multiline}
+                        variant="outlined"
+                        sx={{
+                            "& fieldset": { border: 'none' },
+                            '& .css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input': {
+                                width: '100%',
+                                height: '35px',
+                                fontSize: '20px',
+                                padding: '0px',
+                                textOverflow: 'ellipsis'
+                            }
+                        }}
+                    />
                 </Box>
                 {isHoverField && (
                     <Box sx={{ width: '5%', display: 'flex', gap: '5px', alignItems: 'center', justifyContent: 'center', minwidth: '0' }}>
-
-                        <FieldsIcons title="Increase field" imgPath="assets/images/loupeIcon.png"></FieldsIcons>
-
-                        <FieldsIcons title="Add in archive" imgPath="assets/images/deleteIcon.png"></FieldsIcons>
+                        <Box onClick={handleToggleMultiline}>
+                            <FieldsIcons title="Increase field" imgPath="assets/images/loupeIcon.png"></FieldsIcons>
+                        </Box>
+                        <Box>
+                            <FieldsIcons title="Add in archive" imgPath="assets/images/deleteIcon.png"></FieldsIcons>
+                        </Box>
 
                     </Box>
                 )}
